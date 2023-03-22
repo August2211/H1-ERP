4ee16639495fbb744e2bd452dd36627e342c2e40
Sebastian Veggerby Rasmussen <SFU@kmd.dk>
2023-03-22 10:49:27 +0100

    D5 our SalesOrderHead class with handeling of negative amount handeling

--- H1-ERP/DomainModel/SalesOrderHeader.cs
+++ H1-ERP/DomainModel/SalesOrderHeader.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace H1_ERP.DomainModel
 {
-    internal class SalgsOrderHoved
+    internal enum Condtion
     {
+        None = 0,
+        Created = 1,
+        Confirmed = 2,
+        Pakagede =3,
+        done = 4,
+
+    }
+    public class SalesOrderHeader
+    {
+
+
+        public uint OrderID { get; set; }
+
+        public DateTime Creationtime { get; private set; }
+
+        public DateTime CompletionTime { get;  set; }
+
+        public uint CustomerID { get; set; }
+
+        internal Condtion Condtion { get; set; }
+
+        List<SalesOrderLine> OrderLines { get; set; }
+
+        /// <summary>
+        /// This is a method for calculating the total price of the wholde order
+        /// </summary>
+        /// <returns></returns>
+        public decimal TotalOrderPrice(List<SalesOrderLine> input)
+        {
+            return input.Select(x => x.TotalPrice).ToArray().Sum();
+        }
     }
 }
