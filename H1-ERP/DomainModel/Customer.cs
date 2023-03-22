e12cf84fddc82f7a0a6967c33b1176d7dd2f99f4
August2211 <53059031+August2211@users.noreply.github.com>
2023-03-22 10:28:07 +0100

    Updated Customer class to inherit Person
    D5

--- H1-ERP/DomainModel/Customer.cs
+++ H1-ERP/DomainModel/Customer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace H1_ERP.DomainModel
 {
-    public class Customer
+    public class Customer : Person
     {
         //Customer Properties.
         public int CustomerId { get; set; }
