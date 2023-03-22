f436e8e50647769938a00ff7a2caf88d808bec34
August2211 <53059031+August2211@users.noreply.github.com>
2023-03-22 10:15:31 +0100

    Created Person.cs
    D4

---
+++ H1-ERP/DomainModel/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_ERP.DomainModel
+{
+    public class Person
+    {
+        //Person properties.
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public Address Address { get; set; }
+
+        //Function that returns the full name of a person in "".
+        public string FullName()
+        {
+            return $"\"{FirstName} {LastName}\"";
+        }
+    }
+}
