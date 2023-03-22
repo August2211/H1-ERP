4977abb630937bd8342fa6345a3f62b85477030a
August2211 <53059031+August2211@users.noreply.github.com>
2023-03-22 10:47:49 +0100

    Create ErrorHandling.cs

---
+++ H1-ERP/ErrorHandling/ErrorHandling.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_ERP.ErrorHandling
+{
+    public class ErrorHandling
+    {
+        public void StringIsNullException()
+        {
+            throw new InvalidDataException("This field cannot be null");
+        }
+    }
+}
