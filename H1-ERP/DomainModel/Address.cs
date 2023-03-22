ae4adbf3a00cad9b7969bd88caf0c7aa6a520b94
August2211 <53059031+August2211@users.noreply.github.com>
2023-03-22 11:08:35 +0100

    Added null handling to Address
    D3

--- H1-ERP/DomainModel/Address.cs
+++ H1-ERP/DomainModel/Address.cs
@@ -1,12 +1,76 @@
-﻿namespace H1_ERP.DomainModel
+﻿using H1_ERP.ErrorHandling;
+
+namespace H1_ERP.DomainModel
 {
     public class Address
     {
+
+        //A new instance of ErrorHandling.
+        ErrorHandling.ErrorHandling ErrorHandler = new();
+
         //Address properties.
-        public string RoadName { get; set; }
-        public string StreetNumber { get; set; }
-        public string ZipCode { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
+        private string _roadName;
+        private string _streetNumber;
+        private string _zipCode;
+        private string _city;
+        private string _country;
+
+        public string RoadName
+        {
+            get
+            {
+                //If the road name is null, throw an exception else return the road name.
+                return ErrorHandler.IsNull(_roadName);
+            }
+            set
+            {
+                //If the road name is null, throw an exception else set the road name.
+                _roadName = ErrorHandler.IsNull(value);
+            }
+        }
+        public string StreetNumber
+        {
+            get
+            {
+                return ErrorHandler.IsNull(_streetNumber);
+            }
+            set
+            {
+                _streetNumber = ErrorHandler.IsNull(value);
+            }
+        }
+        public string ZipCode
+        {
+            get
+            {
+                return ErrorHandler.IsNull(_zipCode);
+            }
+            set
+            {
+                _zipCode = ErrorHandler.IsNull(value);
+            }
+        }
+        public string City
+        {
+            get
+            {
+                return ErrorHandler.IsNull(_city);
+            }
+            set
+            {
+                _city = ErrorHandler.IsNull(value);
+            }
+        }
+        public string Country
+        {
+            get
+            {
+                return ErrorHandler.IsNull(_country);
+            }
+            set
+            {
+                _country = ErrorHandler.IsNull(value);
+            }
+        }
     }
 }
