eb09464490a6f9b6a0e8de0734724965d27f588b
Sebastian Veggerby Rasmussen <SFU@kmd.dk>
2023-03-22 10:50:43 +0100

    D7 Our Sales OrderLineClass with null handeling and check for non postive numbers in Price Properties

--- H1-ERP/DomainModel/SalesOrderLine.cs
+++ H1-ERP/DomainModel/SalesOrderLine.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace H1_ERP.DomainModel
 {
-    internal class SalesOrderLine
+    public class SalesOrderLine
     {
+
+        public ushort Quantity { get { return Quantity; }
+            set 
+            {
+                 if(Quantity == 0)
+                 {
+                        throw new Exception("u cannot have a 0 quantatiy of anything"); 
+                 }
+            } 
+        }
+
+       public SalesOrderLine(Product product, ushort quantity) 
+        {
+            Quantity = quantity;
+            TotalPrice = (decimal)(SingleUnitPrice * Quantity);
+            Product = product;
+        }
+
+
+        public decimal? SingleUnitPrice { 
+           
+            get 
+            {
+                if(SingleUnitPrice == null)
+                {
+                    throw new InvalidDataException("Du kan ikke have en pris af null");
+
+                } else { return SingleUnitPrice.Value; }
+            } 
+            set 
+            {
+                if (SingleUnitPrice < 0)
+                {
+                    SingleUnitPrice = null;
+                }
+                else SingleUnitPrice = SingleUnitPrice;
+            }
+        }
+
+        public decimal TotalPrice { get; private set; }
+
+        public Product Product { get;private set; }
+                    
     }
 }
