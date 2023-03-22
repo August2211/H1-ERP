5f2c07ff324c9a9b9e493e45ad71be8d076c0e76
August2211 <53059031+August2211@users.noreply.github.com>
2023-03-22 10:09:43 +0100

    Created Address Class
    D3

---
+++ H1-ERP/DomainModel/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace H1_ERP.DomainModel
+{
+    public class Address
+    {
+        //Address properties.
+        public string RoadName { get; set; }
+        public string StreetNumber { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
