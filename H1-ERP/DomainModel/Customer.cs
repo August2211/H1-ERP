b9de54c11e74174ba4a7261dcd548861c28fc2bd
August2211 <53059031+August2211@users.noreply.github.com>
2023-03-22 10:17:50 +0100

    Created Customer.cs
    D5

---
+++ H1-ERP/DomainModel/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_ERP.DomainModel
+{
+    public class Customer
+    {
+        //Customer Properties.
+        public int CustomerId { get; set; }
+        public DateTime LastPurchaseDate { get; set; }
+    }
+}
