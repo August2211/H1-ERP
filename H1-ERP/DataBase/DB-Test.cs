aa1d45aab188bb8cbf256d7bd1fa4d075443bb3b
August2211 <53059031+August2211@users.noreply.github.com>
2023-04-13 14:55:27 +0200

    Updated DB-Test to reflect new tables

--- H1-ERP/DataBase/DB-Test.cs
+++ H1-ERP/DataBase/DB-Test.cs
@@ -18,16 +18,45 @@ namespace H1_ERP.DataBase
             int Currency = 0; 
             int uniqueNumber = 0;
             int condition = 0;
+
+            string[] SalesNames = new string[10];
+            SalesNames[0] = "Torben";
+            SalesNames[1] = "Kurt";
+            SalesNames[2] = "Søren";
+            SalesNames[3] = "Morten";
+            SalesNames[4] = "Benny";
+            SalesNames[5] = "Lars";
+            SalesNames[6] = "Peter";
+            SalesNames[7] = "Jens";
+            SalesNames[8] = "Edvart";
+            SalesNames[9] = "Lasse";
+
+            string[] SalesSurNames = new string[10];
+            SalesSurNames[0] = "Jensen";
+            SalesSurNames[1] = "Kristensen";
+            SalesSurNames[2] = "Hansen";
+            SalesSurNames[3] = "Nielsen";
+            SalesSurNames[4] = "Petersen";
+            SalesSurNames[5] = "Andersen";
+            SalesSurNames[6] = "Madsen";
+            SalesSurNames[7] = "Larsen";
+            SalesSurNames[8] = "Jørgensen";
+            SalesSurNames[9] = "Pedersen";
+
             for (int i = 0; i < amountToAdd; i++)
             {
                 Exec_SQL_Command($"INSERT INTO [dbo].[Customer.Adress] (RoadName, StreetNumber, ZipCode, City, Country) VALUES('Nej Vej{uniqueNumber}', '123{uniqueNumber}', '321{uniqueNumber}', 'NejCity{uniqueNumber}', 'NejCountry{uniqueNumber}')");
                 var AddressID = GetData("SELECT TOP(1) AdressID FROM [dbo].[Customer.Adress] ORDER BY AdressID desc").Values.ElementAt(0)[0];
-                Exec_SQL_Command($"INSERT INTO [dbo].[Customers.Person] (FirstName, LastName, Email, PhoneNumber, AdressID) VALUES ('John{uniqueNumber}', 'Doe{uniqueNumber}', 'johndoe@example.com{uniqueNumber}', '+123456789{uniqueNumber}', {AddressID})");
+                Exec_SQL_Command($"INSERT INTO [dbo].[Customers.Person] (FirstName, LastName, Email, PhoneNumber, AdressID) VALUES ('John{uniqueNumber}', 'Doe{uniqueNumber}', 'johndoe@gmail.com{uniqueNumber}', '+123456789{uniqueNumber}', {AddressID})");
                 Exec_SQL_Command($"INSERT INTO [dbo].[Company] (CompanyName, Currency, AdressID) VALUES ('CompanyName{uniqueNumber}', {Currency}, {AddressID})");
                 var PersonID = GetData("SELECT TOP(1) PersonID FROM [dbo].[Customers.Person] ORDER BY PersonID desc").Values.ElementAt(0)[0];
                 Exec_SQL_Command($"INSERT INTO [dbo].[Customer.Customers] (LastPurchaseDate, PersonID) VALUES ('2023-03-23 16:03:00', {PersonID})");
+                Exec_SQL_Command($"INSERT INTO [dbo].[Customers.Person] (FirstName, LastName, Email, PhoneNumber, AdressID) VALUES ('{SalesNames[uniqueNumber]}', '{SalesSurNames[uniqueNumber]}', '{SalesNames[uniqueNumber]}{SalesSurNames[uniqueNumber]}@WANK.com{uniqueNumber}', '+123456789{uniqueNumber}', {AddressID})");
+                var EmployeePersonID = GetData("SELECT TOP(1) PersonID FROM [dbo].[Customers.Person] ORDER BY PersonID desc").Values.ElementAt(0)[0];
+                Exec_SQL_Command($"INSERT INTO [dbo].[Company.Employees] VALUES (GETDATE(), GETDATE()+3000, {uniqueNumber * 1000}, {EmployeePersonID})");
                 var CustomerID = GetData("SELECT TOP(1) CustomerID FROM [dbo].[Customer.Customers] ORDER BY CustomerID desc").Values.ElementAt(0)[0];
-                Exec_SQL_Command($"INSERT INTO [dbo].[Sales.Orders] (CustomerID, TotalPriceOfOrder, DateOfOrder, ExpectedDeliveryDate, Comments, Condition) VALUES ({CustomerID}, 5{uniqueNumber}.99, '2023-03-23 16:03:00', '2023-03-30 16:03:00', 'Please deliver to the front door{uniqueNumber}.', {condition})");
+                var EmployeeID = GetData("SELECT TOP(1) Id FROM [dbo].[Company.Employees] ORDER BY Id desc").Values.ElementAt(0)[0];
+                Exec_SQL_Command($"INSERT INTO [dbo].[Sales.Orders] (CustomerID, TotalPriceOfOrder, DateOfOrder, ExpectedDeliveryDate, Comments, Condition, SalesPerson) VALUES ({CustomerID}, 5{uniqueNumber}.99, '2023-03-23 16:03:00', '2023-03-30 16:03:00', 'Please deliver to the front door{uniqueNumber}.', {condition}, '{EmployeeID}')");
                 var OrderID = GetData("SELECT TOP(1) OrderID FROM [dbo].[Sales.Orders] ORDER BY OrderID desc").Values.ElementAt(0)[0];
                 Exec_SQL_Command($"INSERT INTO [dbo].[Product] (ProductName, ProductDescription, ProductSalePrice, ProductPurchasePrice, ProductLocation, ProductQuantity, ProductUnit) VALUES ('Product{uniqueNumber}', 'Description{uniqueNumber}', 2{uniqueNumber}, 3{uniqueNumber}, 'lll{uniqueNumber}', 5{uniqueNumber}, {uniqueNumber})");
                 var ProductID = GetData($"SELECT TOP(1) ProductID FROM [dbo].[Product] ORDER BY ProductID desc").Values.ElementAt(0)[0];
@@ -56,8 +85,9 @@ namespace H1_ERP.DataBase
             Exec_SQL_Command("DELETE FROM [H1PD021123_Gruppe4].[dbo].[Sales.OrderLines]");
             Exec_SQL_Command("DELETE FROM [H1PD021123_Gruppe4].[dbo].[Sales.Orders]");
             Exec_SQL_Command("DELETE FROM [H1PD021123_Gruppe4].[dbo].[Customer.Customers]");
+            Exec_SQL_Command("DELETE FROM [H1PD021123_Gruppe4].[dbo].[Company.Employees]");
             Exec_SQL_Command("DELETE FROM [H1PD021123_Gruppe4].[dbo].[Customers.Person]");
-            Exec_SQL_Command("DELETE FROM [H1PD021123_Gruppe4].[dbo].[Company] " );
+            Exec_SQL_Command("DELETE FROM [H1PD021123_Gruppe4].[dbo].[Company]");
             Exec_SQL_Command("DELETE FROM [H1PD021123_Gruppe4].[dbo].[Customer.Adress]");
             Exec_SQL_Command("DELETE FROM [H1PD021123_Gruppe4].[dbo].[Product]");
         }
