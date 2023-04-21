2b6c723cb48b8064191a861f50bc3ad1e59d3bbc
August2211 <53059031+August2211@users.noreply.github.com>
2023-04-21 11:04:47 +0200

    Fixed dbsales

--- H1-ERP/DataBase/DB-Sales.cs
+++ H1-ERP/DataBase/DB-Sales.cs
@@ -21,11 +21,11 @@ namespace H1_ERP.DataBase
         public SalesOrderHeader GetSalesOrderHeaderFromID(int id)
         {
 
-            var querrydata = GetDatafast($"SELECT * FROM [H1PD021123_Gruppe4].[dbo].[Sales.OrderLines] WHERE OrderID = {id}"); 
-           
+            var querrydata = GetDatafast($"SELECT * FROM [H1PD021123_Gruppe4].[dbo].[Sales.OrderLines] WHERE OrderID = {id}");
+
             List<SalesOrderLine> lines = new List<SalesOrderLine>();
             //Get's all of the orderlines which is connected to the current order. 
-            foreach(var s in querrydata.Values)
+            foreach (var s in querrydata.Values)
             {
                 int GetProdID = (int)s[1];
                 //Calls the method getproductfromid thereby i can instancetiate my object so that i can create an order line.
@@ -38,17 +38,17 @@ namespace H1_ERP.DataBase
             SalesOrderHeader res = new SalesOrderHeader(lines);
             // Lookup in the Order table for the remaning information. 
 
-            var querrydata2 = GetDatafast($"SELECT * FROM [H1PD021123_Gruppe4].[dbo].[Sales.Orders] WHERE OrderID = {id}"); 
+            var querrydata2 = GetDatafast($"SELECT * FROM [H1PD021123_Gruppe4].[dbo].[Sales.Orders] WHERE OrderID = {id}");
 
-            foreach(var s in querrydata2.Values)
+            foreach (var s in querrydata2.Values)
             {
-                  res.OrderID = Convert.ToUInt32(s[0]);
-                  res.CustomerID = Convert.ToUInt32(s[1]);
-                  res.DateOfOrder = Convert.ToDateTime(s[3]);
-                  res.ExpectedDeliveryDate = Convert.ToDateTime(s[4]);
+                res.OrderID = Convert.ToUInt32(s[0]);
+                res.CustomerID = Convert.ToUInt32(s[1]);
+                res.DateOfOrder = Convert.ToDateTime(s[3]);
+                res.ExpectedDeliveryDate = Convert.ToDateTime(s[4]);
             }
-            return res; 
-        }  
+            return res;
+        }
         /// <summary>
         /// Gets all the ids of the current customers and foreach id it finds it calls the method which finds a customer from the id. 
         /// </summary>
@@ -58,11 +58,11 @@ namespace H1_ERP.DataBase
 
             var products = GetdataFastFromJoinsWithouttheKeyvalueparoftheId($"SELECT * FROM [H1PD021123_Gruppe4].[dbo].[Sales.Orders] INNER Join [H1PD021123_Gruppe4].[dbo].[Sales.OrderLines]" +
                 $"on [dbo].[Sales.OrderLines].OrderID = [dbo].[Sales.Orders].OrderID Inner join  [H1PD021123_Gruppe4].[dbo].[Product]" +
-                $"on [dbo].[Product].ProductID =  [dbo].[Sales.OrderLines].ProductID"); 
+                $"on [dbo].[Product].ProductID =  [dbo].[Sales.OrderLines].ProductID");
             List<SalesOrderHeader> res = new List<SalesOrderHeader>();
 
 
-            foreach(var orderheaderline in products.Values)
+            foreach (var orderheaderline in products.Values)
             {
                 var checklist = res.Where(x => x.OrderID == Convert.ToUInt32(orderheaderline[0])).ToList();
                 if (checklist.Count == 0)
@@ -75,7 +75,7 @@ namespace H1_ERP.DataBase
                         //we select the product with the matching ID 
                         var prod = products.Values.Select(x => x).Where(x => x.ElementAt(9).Equals(orderline[9])).FirstOrDefault();
                         Product product = new Product();
-                        product.ProductID = Convert.ToInt32(prod[9]);
+                        product.ProductId = Convert.ToInt32(prod[9]);
                         product.ProductName = prod[15].ToString();
                         product.ProductDescription = prod[16].ToString();
                         product.ProductSalePrice = Convert.ToInt32(prod[17]);
@@ -84,7 +84,7 @@ namespace H1_ERP.DataBase
                         product.ProductQuantity = Convert.ToInt32(prod[20]);
                         product.ProductUnit = Convert.ToInt32(prod[21]);
                         SalesOrderLine orderLine = new SalesOrderLine(product, Convert.ToUInt16(prod[20].ToString()));
-                        orderLine.OrderLineID = Convert.ToUInt32(orderline[8]);
+                        orderLine.Id = Convert.ToUInt32(orderline[8]);
                         orderLine.Quantity = Convert.ToUInt16(orderline[20]);
                         line.Add(orderLine);
                     }
@@ -92,24 +92,24 @@ namespace H1_ERP.DataBase
                     salesOrder.OrderID = Convert.ToUInt32(orderheaderline[0]);
                     salesOrder.CustomerID = Convert.ToUInt32(orderheaderline[1]);
                     salesOrder.TotalOrderPrice();
-                    salesOrder.DateOfOrder = Convert.ToDateTime(orderheaderline[3]);
-                    salesOrder.ExpectedDeliveryDate = Convert.ToDateTime((DateTime)orderheaderline[4]);
+                    salesOrder.Creationtime = Convert.ToDateTime(orderheaderline[3]);
+                    salesOrder.CompletionTime = Convert.ToDateTime((DateTime)orderheaderline[4]);
                     salesOrder.Condtion = (Condtion)Convert.ToInt32(orderheaderline[6]);
                     res.Add(salesOrder);
                 }
             }
-            return res; 
+            return res;
         }
         /// <summary>
         /// Inserts a sales ordereheader from an new instance of the obejct 
         /// </summary>
         /// <param name="input"></param>
         public void InsertSalesOrderHeader(SalesOrderHeader input)
-        { 
-            Exec_SQL_Command($"INSERT INTO [H1PD021123_Gruppe4].[dbo].[Sales.Orders] (CustomerID,TotalPriceOfOrder,DateOfOrder,ExpectedDeliveryDate) VALUES({input.CustomerID},{input.TotalOrderPrice()},{input.DateOfOrder},{input.ExpectedDeliveryDate})");
+        {
+            Exec_SQL_Command($"INSERT INTO [H1PD021123_Gruppe4].[dbo].[Sales.Orders] (CustomerID,TotalPriceOfOrder,DateOfOrder,ExpectedDeliveryDate) VALUES({input.CustomerID},{input.TotalOrderPrice()},{input.Creationtime},{input.CompletionTime})");
             //find the id of the inserted header by looking at the highest ID
             var Data = GetData("SELECT TOP (1) [OrderID] FROM [H1PD021123_Gruppe4].[dbo].[Sales.Orders] ORDER BY [OrderID] desc;");
-            int OrderID = 1; 
+            int OrderID = 1;
             foreach (var Row in Data.Values)
             {
                 OrderID = Convert.ToInt32(Row[0]);
@@ -117,7 +117,7 @@ namespace H1_ERP.DataBase
             //InsertSalesOrderHeader all of the Orderlines in the table orderlines
             foreach (var OrderLine in input.OrderLines)
             {
-                Exec_SQL_Command($"INSERT INTO [H1PD021123_Gruppe4].[dbo].[Sales.OrderLines] (SinglePrice,OrderQuantity,TotalQuantityPrice,OrderID,ProductID) VALUES({OrderLine.SinglePrice},{OrderLine.Quantity},{OrderLine.TotalQuantityPrice},{OrderID},{OrderLine.Product.ProductID})");
+                Exec_SQL_Command($"INSERT INTO [H1PD021123_Gruppe4].[dbo].[Sales.OrderLines] (SinglePrice,OrderQuantity,TotalQuantityPrice,OrderID,ProductID) VALUES({OrderLine.SingleUnitPrice},{OrderLine.Quantity},{OrderLine.TotalPrice},{OrderID},{OrderLine.Product.ProductId})");
             }
         }
         /// <summary>
@@ -130,7 +130,7 @@ namespace H1_ERP.DataBase
             string sqlCommands = "";
             foreach (var OrderLines in NewsalesHeader.OrderLines)
             {
-                sqlCommands += $"UPDATE [H1PD021123_Gruppe4].[dbo].[Sales.OrderLines] SET SinglePrice = {OrderLines.SingleUnitPrice.ToString().Replace(',','.')}, OrderQuantity = {OrderLines.Quantity.ToString().Replace(',', '.')}, TotalQuantityPrice = {OrderLines.TotalPrice.ToString().Replace(',', '.')} WHERE OrderID = {ID} AND OrderLineID = {OrderLines.Id};\n";
+                sqlCommands += $"UPDATE [H1PD021123_Gruppe4].[dbo].[Sales.OrderLines] SET SinglePrice = {OrderLines.SingleUnitPrice.ToString().Replace(',', '.')}, OrderQuantity = {OrderLines.Quantity.ToString().Replace(',', '.')}, TotalQuantityPrice = {OrderLines.TotalPrice.ToString().Replace(',', '.')} WHERE OrderID = {ID} AND OrderLineID = {OrderLines.Id};\n";
             }
             sqlCommands = $"SET DATEFORMAT dmy;\nUPDATE [H1PD021123_Gruppe4].[dbo].[Sales.Orders] SET TotalPriceOfOrder = {NewsalesHeader.TotalOrderPrice().ToString().Replace(',', '.')}, ExpectedDeliveryDate = CONVERT(datetime,'{NewsalesHeader.CompletionTime}', 103) WHERE OrderID = {ID};\n" + sqlCommands;
             Exec_SQL_Command(sqlCommands);
@@ -147,13 +147,13 @@ namespace H1_ERP.DataBase
 
         public SalesOrderHeader GetFromID(int id)
         {
-            return GetSalesOrderHeaderFromID(id); 
+            return GetSalesOrderHeaderFromID(id);
 
         }
 
         public List<SalesOrderHeader> GetAll(int id)
         {
-            return GetAll(); 
+            return GetAll();
         }
 
         public void Delete(int id)
@@ -163,12 +163,12 @@ namespace H1_ERP.DataBase
 
         public void Update(SalesOrderHeader enity)
         {
-            UpdateSalesorderHeader((int)enity.OrderID, enity); 
+            UpdateSalesorderHeader((int)enity.OrderID, enity);
         }
 
         public void Insert(SalesOrderHeader entity)
         {
-            InsertSalesOrderHeader(entity); 
+            InsertSalesOrderHeader(entity);
         }
     }
 }
