c8bbea8c20dbeb2bc6a64f0139a5afc4669c84c0
August2211 <53059031+August2211@users.noreply.github.com>
2023-04-21 11:55:31 +0200

    Fixed merge conflict

---
+++ H1-ERP/DapperDB/Dapper-DB-Company.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using H1_ERP.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_ERP.DapperDB
+{
+    public partial class DatabaseDapper
+    {
+        public Company GetCompanyFromID(int ID)
+        {
+            using (var conn = getConnection())
+            {
+                var query = conn.QuerySingle<Company>($"SELECT * FROM [H1PD021123_Gruppe4].[dbo].[Company] WHERE CompanyID = {ID}");
+
+                return query;
+            }
+        }
+    }
+}
