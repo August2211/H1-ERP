cd151e0971aeac34a043af4d4851c898b39af4f8
Sebastian Veggerby Rasmussen <Sebastian.veggerby@gmail.com>
2023-05-10 08:54:05 +0200

    Comments added to DB classes

--- H1-ERP/DataBase/DB-Company.cs
+++ H1-ERP/DataBase/DB-Company.cs
@@ -14,13 +14,16 @@ namespace H1_ERP.DataBase
         public Company GetCompanyFromID(int id)
         {
 
+            //we intizialise the individual objects needed for the Company object
             Company company = new Company();
             company.CompanyID = id;
             Address address = new Address();
-            SqlConnection conn = getConnection();
+
+            // we get data from 2 table's using a inner join  
             var SelectedCompany = GetData($"SELECT * FROM [H1PD021123_Gruppe4].[dbo].[Company] INNER JOIN [Customer.Adress] ON " +
                 $"[Customer.Adress].AdressID = Company.AdressID " +
-                $"WHERE CompanyID = {id}", conn);
+                $"WHERE CompanyID = {id}");
+            // we assign each value to our obejct 
             company.CompanyID = Convert.ToInt32(SelectedCompany.ElementAt(0).Value[0]);
             company.CompanyName = SelectedCompany.ElementAt(0).Value[1].ToString();
             company.Currency = (Company.currency)Convert.ToInt32(SelectedCompany.ElementAt(0).Value[2]);
@@ -33,21 +36,22 @@ namespace H1_ERP.DataBase
             address.City = SelectedCompany.ElementAt(0).Value[8].ToString();
             address.Country = SelectedCompany.ElementAt(0).Value[9].ToString();
 
-
+            //we make a check to ensure the company has an adress
             if (address.AdressID != 0)
             {
                 company.Address = address;
             }
-            conn.Close();
             return company;
         }
         //Get all Company and return it to the list 
         public List<Company> GetAllCompany()
         {
-            SqlConnection connection = getConnection();
             List<Company> result = new List<Company>();
+            //Get the data from the 2 table's and join them together 
             var AllCompanies = GetData("SELECT * FROM [dbo].[Company] INNER JOIN [dbo].[Customer.Adress] ON [dbo].[Company].AdressID = [dbo].[Customer.Adress].AdressID");
 
+
+            // after the data is joined we take the indvidual value's and set them for our indvidual objects
             foreach (var row in AllCompanies.Values)
             {
                 Company companies = new Company();
@@ -65,42 +69,37 @@ namespace H1_ERP.DataBase
                 companies.Address = address;
                 result.Add(companies);
             }
-            connection.Close();
+            // we return the given company in the database 
             return result;
 
         }  //InsertSalesOrderHeader the Company and put it in dataBase
         public void InputCompany(Company Input)
         {
-            SqlConnection connection = getConnection();
-
+            // We Insert the data for a new adress in the table and afterwards we take select the ID for the given adress and insert in to the company table 
             var AddressID = GetData($"INSERT INTO [H1PD021123_Gruppe4].[dbo].[Customer.Adress] (RoadName,StreetNumber,ZipCode,City,Country) VALUES ('{Input.Address.RoadName}','{Input.Address.StreetNumber}', '{Input.Address.ZipCode}','{Input.Address.City}', '{Input.Address.Country}') " +
                 $"SELECT TOP(1) AdressID FROM [dbo].[Customer.Adress] ORDER BY AdressID desc").Values.ElementAt(0)[0];
 
-            string sql = $"INSERT INTO [H1PD021123_Gruppe4].[dbo].[Company](CompanyName,Currency,AdressID) VALUES ('{Input.CompanyName}','{(int)Input.Currency}','{AddressID}')";
-            SqlCommand sqlCommand = new SqlCommand(sql, connection);
-            sqlCommand.ExecuteNonQuery();
-            connection.Close();
+           Exec_SQL_Command($"INSERT INTO [H1PD021123_Gruppe4].[dbo].[Company](CompanyName,Currency,AdressID) VALUES ('{Input.CompanyName}','{(int)Input.Currency}','{AddressID}')");
         }
         //Updata the Company in dataBase
         public void UpdateCompany(Company Input)
         {
-            SqlConnection connection = getConnection();
-            string sql = $"UPDATE [H1PD021123_Gruppe4].[dbo].[Customer.Adress] set RoadName = '{Input.Address.RoadName}',  StreetNumber='{Input.Address.StreetNumber}', ZipCode='{Input.Address.ZipCode}', City= '{Input.Address.City}', Country='{Input.Address.Country}' WHERE AdressID='{Input.Address.AdressID}' " +
-                         $"UPDATE [H1PD021123_Gruppe4].[dbo].[Company] set CompanyName = '{Input.CompanyName}',  Currency='{((int)Input.Currency)}', AdressID='{Input.Address.AdressID}'WHERE CompanyID='{Input.CompanyID}'";
-            SqlCommand sqlCommand = new SqlCommand(sql, connection);
-            sqlCommand.ExecuteNonQuery();
-            connection.Close();
+            // we take the company obejct and update all the values in the database 
+            Exec_SQL_Command($"UPDATE [H1PD021123_Gruppe4].[dbo].[Customer.Adress] set RoadName = '{Input.Address.RoadName}',  StreetNumber='{Input.Address.StreetNumber}'," +
+                             $" ZipCode='{Input.Address.ZipCode}', City= '{Input.Address.City}', Country='{Input.Address.Country}' WHERE AdressID='{Input.Address.AdressID}' " +
+                            $"UPDATE [H1PD021123_Gruppe4].[dbo].[Company] set CompanyName = '{Input.CompanyName}',  Currency='{((int)Input.Currency)}', 0dressID='{Input.Address.AdressID}'WHERE CompanyID='{Input.CompanyID}'"); 
         }
         //DeleteSalesOrderHeaderFromID the Company from dataBase
 
         public void DeleteCompany(int Input)
         {
-            SqlConnection connection = getConnection();
-            string sql = $"DELETE FROM [H1PD021123_Gruppe4].[dbo].[Company] WHERE CompanyID = {Input}";
+           Company company = new Company();
+            // Firstly we get the adressID from the database 
+            company.Address.AdressID = (uint)GetData($"SELECT AdressID FROM [H1PD021123_Gruppe4].[dbo].[Company] WHERE CompanyID = {Input}").ElementAt(0).Value[0];
+            //delete in the order beneath to avoid errors
+            Exec_SQL_Command($"DELETE FROM [H1PD021123_Gruppe4].[dbo].[Company] WHERE CompanyID = {Input}");
+            Exec_SQL_Command($"DELETE FROM [H1PD021123_Gruppe4].[dbo].[Customer.Adress] WHERE AdressID = {company.Address.AdressID}");
 
-            SqlCommand sqlCommand = new SqlCommand(sql, connection);
-            sqlCommand.ExecuteNonQuery();
-            connection.Close();
         }
     }
 }
