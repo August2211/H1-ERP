ae2042b7ba9a1d0d6c7f3f9dbc5d3aef41d3d254
August2211 <53059031+August2211@users.noreply.github.com>
2023-05-10 10:08:28 +0200

    Changed deleteproduct to use the correct method

--- H1-ERP/Display/DisplayProduct.cs
+++ H1-ERP/Display/DisplayProduct.cs
@@ -99,8 +99,8 @@ namespace H1_ERP.Display
 
             Action<ProductDisplay> removeFunction = delegate (ProductDisplay product)
             {
-                var data = dataBase.GetDatafast($"DELETE FROM [dbo].[Sales.OrderLines] WHERE ProductID = {product.Title2}" +
-                    $"DELETE FROM [dbo].[Product] WHERE ProductID = {product.Title2}");
+                dataBase.Exec_SQL_Command($"DELETE FROM [dbo].[Sales.OrderLines] WHERE ProductID = {product.Title2}" +
+                $"DELETE FROM [dbo].[Product] WHERE ProductID = {product.Title2}");
             };
 
             Action<ProductDisplay> addFunction = delegate (ProductDisplay product)
